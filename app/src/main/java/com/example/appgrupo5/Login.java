bd9f68b145a11143f1a29a2b017a66f709cdde19
CRISTIAN CAMILO GONZALEZ RAMIREZ <42184966+kamilo1991@users.noreply.github.com>
2021-12-01 23:02:52 -0500

    menu
    menu app

--- app/src/main/java/com/example/appgrupo5/Login.java
+++ app/src/main/java/com/example/appgrupo5/Login.java
@@ -8,19 +8,27 @@ import android.widget.Button;
 
 public class Login extends AppCompatActivity {
     Button btn_registrar;
-
+    Button btn_iniciar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         btn_registrar=findViewById(R.id.btn_registrar);
+        btn_iniciar=findViewById(R.id.btn_iniciar);
 
         btn_registrar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intentreg= new Intent(Login.this, Registro.class);
                 Login.this.startActivity(intentreg);
+            }
+        });
+        btn_iniciar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intemenu= new Intent(Login.this, Menu.class);
+                Login.this.startActivity(intemenu);
             }
         });
     }
